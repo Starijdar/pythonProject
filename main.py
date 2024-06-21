e46747428a2220cf690de01598abe22b0be1ae6f
Stardar <stardar725@gmail.com>
2024-06-21 22:28:28 +0300

    Папка с практическими

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Hello world")
+a = "Съешь ещё этих мягких французских булок, да выпей же чаю"
+print(len(a))
+first = 10
+second = 4
+summa = first + second
+diff = first - second
+print(summa, diff)
+third = 4
+mean = (first + second + third) / 3
+print(mean)
+first_string = "Понедельник"
+second_string = "Вторник"
+print(first_string, ", ", second_string)
+afl = 5
+bfl = 2
+cfl = 8
+f = (afl * bfl) + (afl * cfl)
+print((f ** 3) / 2)
+name = "Ivan"
+print(str(afl) + " " + name + name[::-1])
