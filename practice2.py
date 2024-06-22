87759ed4df2efc6483a275ab856735217b66d0a6
Stardar <stardar725@gmail.com>
2024-06-22 22:52:30 +0300

    Папка с практическими

---
+++ practice2.py
@@ -0,0 +1,5 @@
+tasks_count = 12
+hours_spent = 1.5
+course_name = "Python"
+task_time = hours_spent / tasks_count
+print("Курс:" , course_name , ", всего задач:", tasks_count , ", затрачено часов:" , hours_spent , ", среднее время выполнения:" , task_time)
