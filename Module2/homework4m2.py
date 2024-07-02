a05cd161e1f965833f476a26d2399416f30ba3cc
Stardar <stardar725@gmail.com>
2024-07-02 23:44:51 +0300

    M2P4

--- Module2/homework4m2.py
+++ Module2/homework4m2.py
@@ -1,6 +1,7 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
+
 for i in range(len(numbers)):
     test = numbers[i]
     if test == 1:
@@ -9,11 +10,9 @@ for i in range(len(numbers)):
     for k in range(1, test + 1):
         if test % k == 0:
             z += 1
+        if z > 2:
+            break
     if z == 2:
-        is_prime = True
-    else:
-        is_prime = False
-    if is_prime:
         primes.append(test)
     else:
         not_primes.append(test)
