4121af1e868a310b5ab0fc4d26ab2921d733663c
Stardar <stardar725@gmail.com>
2024-07-02 21:31:06 +0300

    M2P3

--- Module2/homework3m2.py
+++ Module2/homework3m2.py
@@ -1,7 +1,7 @@
-my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = 0
-while my_list[i] > -1:
-    if i >= len(my_list):
+while i <= len(my_list):
+    if my_list[i] < 0:
         break
     else:
         if my_list[i] > 0:
