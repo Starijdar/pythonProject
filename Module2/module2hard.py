cc933c83991e9c4fc0b44b30e8c36ff8dd95f1b9
Stardar <stardar725@gmail.com>
2024-07-04 22:00:55 +0300

    Основное решение M2 Hard

--- Module2/module2hard.py
+++ Module2/module2hard.py
@@ -1,17 +1,3 @@
-# n = int(input("Число в первой ячейке "))
-# for i in range(1, n):
-#     k = 0
-#     for k in range(1, n):
-#         if k == i:
-#             continue
-#         else:
-#             if n % (i + k) == 0:
-#                 print(i, k)
-
-
-
-
-
 result = []
 
 n = int(input("Число в первой ячейке "))
@@ -27,20 +13,7 @@ for i in range(1, n):
 
 # result_string.replace("[ ]", "") 12 13 15 111 24 210 39 48 57
 
-result_string = result
 print(*result)
 
-result_string = str(result_string).translate({ord(i): None for i in ", '[]"})
+result_string = str(result).translate({ord(i): None for i in ", '[]"})
 print(result_string)  #Приведение к результату примера
-
-
-
-
-
-
-
-
-# #20 - 13 14 19 119 23 28 218 37 317 46 416 515 614 713 812 911
-# #10 - 14 19 23 28 37 46
-# for x in result:
-#     if sum(result[])
