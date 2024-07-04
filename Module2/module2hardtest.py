522dda62deb3b725054c05315c2c330439e8c3f3
Stardar <stardar725@gmail.com>
2024-07-04 21:58:43 +0300

    Решение c def

---
+++ Module2/module2hardtest.py
@@ -0,0 +1,21 @@
+def motocikl(result, i, k, n):
+    for i in range(1, n):
+        if i > n / 2:
+            break
+        for k in range(i + 1, n):
+            if k == i:
+                continue
+            else:
+                if n % (i + k) == 0:
+                    result.append(f'{i}{k}')
+    return result
+
+
+result = []
+i = 0
+k = 0
+n = int(input("Число в первой ячейке "))
+
+
+motocikl(result, i, k, n)
+print(*result)
