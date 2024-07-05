2bf7ec3512c1a2a294f79456ca2d5e192e54d35e
Stardar <stardar725@gmail.com>
2024-07-05 18:47:50 +0300

    Module3w1

---
+++ Module3/module3_1.py
@@ -0,0 +1,26 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+    return calls
+
+def string_info(string):
+    string = (len(string), string.upper(), string.lower())
+    count_calls()
+    return string
+
+def is_contains(string, list_to_search):
+    count_calls()
+    for i in list_to_search:
+        if string.lower() == i.lower():
+            return True
+    return False
+
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
