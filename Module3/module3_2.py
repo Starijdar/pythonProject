d16c18664872de4354d8104dc719efd3d47ac316
Stardar <stardar725@gmail.com>
2024-07-05 20:00:19 +0300

    Module3_2

--- Module3/module3_2.py
+++ Module3/module3_2.py
@@ -14,8 +14,6 @@ def send_email(message, recipient, *, sender="university.help@gmail.com"):
         print("Нелзья отправлять сообщение самому себе")
         return
 
-
-
     if count >= 3:
         if sender == "university.help@gmail.com":
             print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
@@ -23,7 +21,6 @@ def send_email(message, recipient, *, sender="university.help@gmail.com"):
             print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
     else:
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
