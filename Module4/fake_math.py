fb2fe36f699689eb0bd32d1209ff303f5e56e7f8
Stardar <stardar725@gmail.com>
2024-07-21 18:57:10 +0300

    module4_1

---
+++ Module4/fake_math.py
@@ -0,0 +1,5 @@
+def divide(first, second):
+    if second == 0:
+        return 'ERROR'
+    else:
+        return (first / second)
---
+++ Module4/module4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as fakediv
+from true_math import divide as truediv
+
+result1 = fakediv(69, 3)
+result2 = fakediv(3, 0)
+result3 = truediv(49, 7)
+result4 = truediv(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
