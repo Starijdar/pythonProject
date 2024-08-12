b5c333d396a808fadf1ac71f8783af8c3572147a
Stardar <stardar725@gmail.com>
2024-08-12 19:15:39 +0300

    Module6 3

---
+++ Module6/Module6_3.py
@@ -0,0 +1,36 @@
+class Horse:
+    x_distance = 0
+    sound = 'Frrr'
+    def run(self, dx):
+        self.x_distance += dx
+        return self.x_distance
+
+class Eagle:
+    y_distance = 0
+    sound = 'I train, eat, sleep, and repeat, Khabib reference?'
+    def fly(self, dy):
+        self.y_distance += dy
+        return self.y_distance
+
+class Pegasus(Horse, Eagle):
+    def move(self, dx, dy):
+        super().run(dx)
+        super().fly(dy)
+
+    def get_pos(self):
+        return (self.x_distance, self.y_distance)
+
+    def voice(self):
+        print(super().sound)
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
+
+print(Pegasus.mro())
