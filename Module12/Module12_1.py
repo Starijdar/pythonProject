ca7024e99b0712482343668d2d48815468f50ec2
Stardar <stardar725@gmail.com>
2024-12-15 18:50:06 +0300

    Module 12 1

---
+++ Module12/Module12_1.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
+
+
+class RunnerTest(TestCase):
+
+    def test_walk(self):
+        walker = Runner('walker')
+        for i in range(0, 10):
+            walker.walk()
+        self.assertEqual(walker.distance, 50)
+
+    def test_run(self):
+        runner = Runner('Runner1')
+        for i in range(0, 10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        contestant1 = Runner('Cont1')
+        contestant2 = Runner('Cont2')
+        for i in range(0, 10):
+            contestant1.walk()
+            contestant2.run()
+        self.assertNotEqual(contestant2.distance, contestant1.distance)
